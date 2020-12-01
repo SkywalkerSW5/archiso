7d0f19053ee718aee7394c3a62f6b17cda313e78
SkywalkerSW5 <skywalkersw5@hotmail.com>
2020-12-01 21:30:13 +0000

    Added GNOME, Firefox, and more programs

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ echo "Cleaning work and ISO dirs..."
 sudo rm -r ./archiso/iso 2> ./error.log
 sudo rm -r ./archiso/work 2> ./error.log
 mkdir ./archiso/work
-mkdir ./archiso/iso
-sudo mkarchiso -v -o ./archiso/iso/ -w ./archiso/iso/ -C ./archiso/pacman.conf ~/.stuff/archiso/archiso
+mkdir ./archiso/iso 
+sudo mkarchiso -v -o ./archiso/iso/ -w ./archiso/work/  ~/.stuff/archiso/archiso 2> ./error.log
+echo "ISO done"
